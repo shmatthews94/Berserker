a1e7a0a634d102a6632d0ab6ebbf75f4bba2835a
Elise Weiland <elise.weiland@gmail.com>
2015-05-31 19:26:20 -0400

    abstract attack class

--- Berserker/PlatformerMac/Attack.cs
+++ Berserker/PlatformerMac/Attack.cs
@@ -82,10 +82,145 @@ namespace PlatformerMac
 					break;
 				case AttackStage.Complete:
 					break;
-
-
 			}
 		}
+
+		protected virtual bool IsReadyForNextStage
+		{
+			get
+			{
+				switch (current)
+				{
+				case AttackStage.Preparing:
+					break;
+				case AttackStage.Advancing:
+					break;
+				case AttackStage.Executing:
+					break;
+				case AttackStage.Returning:
+					break;
+				case AttackStage.Finishing:
+					break;
+
+				}
+			}
+
+			return true;
+		}
+
+		#endregion
+
+		#region Enemy
+
+		protected Enemy enemy;
+
+		public Enemy Enemy
+		{
+			get { return enemy; }
+		}
+
+		public virtual bool IsCharacterValidUser
+		{
+			get { return true; }
+		}
+
+		#endregion
+
+		#region Target
+
+		public Enemy Target = null;
+
+		protected int adjacentTargets = 0;
+
+		public int AdjacentTargets
+		{
+			get { return adjacentTargets; }
+		}
+
+		#endregion
+
+		#region Heuristic
+		/// <summary>
+		/// Gets the heuristic.
+		/// </summary>
+		/// <value>The heuristic.</value>
+		/// <remarks>
+		/// I am unsure if we actually need this part as of yet.
+		/// </remarks>
+		public abstract int Heuristic
+		{
+			get;
+		}
+
+		public static int CompareAttacksByHeuristic(Attack a, Attack b)
+		{
+			return b.Heuristic.CompareTo(a.Heuristic);
+		}
+
+		#endregion
+
+		#region Initialization
+		public Attack(Enemy enemy)
+		{
+			if (enemy == null)
+			{
+				throw new ArgumentNullException("enemy");
+			}
+
+			this.enemy = enemy;
+
+			Reset();
+		}
+
+		public virtual void Reset()
+		{
+			current = AttackStage.NotStarted;
+		}
+
+		public virtual void Start(){
+			current = AttackStage.Preparing;
+			AdvanceStage();
+		}
+
+		#endregion
+
+		#region Updating
+
+		public virtual void Update(GameTime gameTime)
+		{
+			UpdateCurrentStage(gameTime);
+
+			if((current != AttackStage.NotStarted) && (current != AttackStage.Complete) && IsReadyForNextStage)
+			{
+				switch (current)
+				{
+					case AttackStage.Preparing:
+						current = AttackStage.Advancing;
+						break;
+					case AttackStage.Advancing:
+						current = AttackStage.Executing;
+						break;
+					case AttackStage.Executing:
+						current = AttackStage.Returning;
+						break;
+					case AttackStage.Returning:
+						current = AttackStage.Finishing;
+						break;
+					case AttackStage.Finishing:
+						current = AttackStage.Complete;
+						break;
+				}
+				AdvanceStage();
+			}
+		}
+
+		#endregion
+
+		#region Drawing
+
+		public virtual void Draw(GameTime gameTime, SpriteBatch spriteBatch){}
+
+		#endregion
 	}
 }
 
