91dfd370bd3cf2f0ce6e394e400af353a3f5b684
Elise Weiland <elise.weiland@gmail.com>
2015-05-31 19:26:10 -0400

    abstract attack class

---
+++ Berserker/PlatformerMac/Attack.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PlatformerMac
+{
+	/// <summary>
+	/// Attack or defensive action taken by player (using WASD keys)
+	/// </summary>
+	abstract class Attack
+	{
+		#region ActionState
+		public abstract bool isOffensive
+		{
+			get;
+		}
+
+		public abstract bool isTargetNeeded
+		{
+			get;
+		}
+		#endregion
+
+		#region Stage
+		/// <summary>
+		/// Stage of execution for each attack
+		/// </summary>
+
+		public enum AttackStage
+		{
+			NotStarted,
+			Preparing,
+			Advancing,
+			Executing,
+			Returning,
+			Finishing,
+			Complete,
+		};
+
+		protected AttackStage current = AttackStage.NotStarted;
+
+		public AttackStage CurrentStage
+		{
+			get { return current; }
+		}
+
+		protected virtual void AdvanceStage()
+		{
+			switch (current)
+			{
+				case AttackStage.Preparing:
+					break;
+				case AttackStage.Advancing:
+					break;
+				case AttackStage.Executing:
+					break;
+				case AttackStage.Returning:
+					break;
+				case AttackStage.Finishing:
+					break;
+				case AttackStage.Complete:
+					break;
+				
+			}
+		}
+
+		protected virtual void UpdateCurrentStage(GameTime gameTime)
+		{
+			switch(current)
+			{
+				case AttackStage.NotStarted;
+					break;
+				case AttackStage.Preparing:
+					break;
+				case AttackStage.Advancing:
+					break;
+				case AttackStage.Executing:
+					break;
+				case AttackStage.Returning:
+					break;
+				case AttackStage.Finishing:
+					break;
+				case AttackStage.Complete:
+					break;
+
+
+			}
+		}
+	}
+}
+
